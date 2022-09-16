a52c6e0d697877792ca65a2389b65b941e4c6ce7
ReinTristan <zetaeugene@gmail.com>
2022-09-15 21:27:01 -0500

    app finished, fixed minor css bugs

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import './navigation.js'
 import {
 	categoriesPreviewList,
 	categoriesPreviewSection,
+	genericSection,
+	headerSection,
+	movieDetailCategoriesList,
+	movieDetailDescription,
+	movieDetailScore,
+	movieDetailTitle,
+	relatedMoviesContainer,
 	trendingMoviesPreviewList,
 } from './nodes.js'
 
-const IMG_BASE = 'https://image.tmdb.org/t/p/w300'
+const IMG_BASE = 'https://image.tmdb.org/t/p/'
 const api = axios.create({
 	baseURL: 'https://api.themoviedb.org/3/',
 	headers: {
@@ -17,27 +24,37 @@ const api = axios.create({
 	},
 })
 
-async function getTrendingMoviesPreview() {
-	const { data } = await api(`trending/movie/day`)
-	const movies = data.results
-	movies.forEach((movie) => {
+// utils
+
+/**
+ * @param {any[]} movieList Array with the movie list
+ * @param {HTMLElement} container DOM container to append
+ */
+
+function createMovies(movieList, container) {
+	container.innerHTML = ''
+	movieList.forEach((movie) => {
 		const movieContainer = document.createElement('div')
 		movieContainer.classList.add('movie-container')
+		movieContainer.addEventListener('click', () => {
+			location.hash = `#movie=${movie.id}`
+		})
 
 		const movieImg = document.createElement('img')
 		movieImg.classList.add('movie-img')
 		movieImg.setAttribute('alt', movie.title)
-		movieImg.setAttribute('src', `${IMG_BASE}${movie.poster_path}`)
+		movieImg.setAttribute('src', `${IMG_BASE}w300/${movie.poster_path}`)
 
 		movieContainer.append(movieImg)
-		trendingMoviesPreviewList.append(movieContainer)
+		container.append(movieContainer)
 	})
 }
-
-async function getTrendingCategoriesPreview() {
-	const { data } = await api(`genre/movie/list`)
-	const categories = data.genres
-
+/**
+ * @param {any[]} movieList Array with the movie list
+ * @param {HTMLElement} container DOM container to append
+ */
+function createCategories(categories, container) {
+	container.innerHTML = ''
 	categories.forEach((category) => {
 		const categoryContainer = document.createElement('div')
 		categoryContainer.classList.add('category-container')
@@ -45,13 +62,85 @@ async function getTrendingCategoriesPreview() {
 		const categoryTitle = document.createElement('h3')
 		categoryTitle.classList.add('category-title')
 		categoryTitle.setAttribute('id', `id${category.id}`)
+		categoryTitle.addEventListener(
+			'click',
+			() => (location.hash = `#category=${category.id}-${category.name}`)
+		)
 
 		const categoryTitleText = document.createTextNode(category.name)
 
 		categoryTitle.append(categoryTitleText)
 		categoryContainer.append(categoryTitle)
-		categoriesPreviewList.append(categoryContainer)
+		container.append(categoryContainer)
 	})
 }
+//api calls
 
-export { getTrendingCategoriesPreview, getTrendingMoviesPreview }
+async function getTrendingMoviesPreview() {
+	const { data } = await api(`trending/movie/day`)
+	const movies = data.results
+	createMovies(movies, trendingMoviesPreviewList)
+}
+
+async function getTrendingCategoriesPreview() {
+	const { data } = await api(`genre/movie/list`)
+	const categories = data.genres
+	createCategories(categories, categoriesPreviewList)
+}
+async function getMoviesByCategory(id) {
+	const { data } = await api(`discover/movie`, {
+		params: {
+			with_genres: id,
+		},
+	})
+	const movies = data.results
+	createMovies(movies, genericSection)
+}
+
+async function getMoviesBySearch(query) {
+	const { data } = await api(`search/movie`, {
+		params: {
+			query,
+		},
+	})
+	const movies = data.results
+	createMovies(movies, genericSection)
+}
+
+async function getTrendingMovies() {
+	const { data } = await api(`trending/movie/day`)
+	const movies = data.results
+	createMovies(movies, genericSection)
+}
+async function getMovieById(id) {
+	const { data: movie } = await api(`movie/${id}`)
+	const movieImgUrl = `${IMG_BASE}w500/${movie.poster_path}`
+
+	headerSection.style.background = `linear-gradient(
+		180deg,
+		rgba(0, 0, 0, 0.35) 19.27%,
+		rgba(0, 0, 0, 0) 29.17%
+	),url(${movieImgUrl})`
+
+	movieDetailTitle.textContent = movie.title
+	movieDetailDescription.textContent = movie.overview
+	movieDetailScore.textContent = parseFloat(movie.vote_average).toFixed(1)
+
+	createCategories(movie.genres, movieDetailCategoriesList)
+	getRelatedMoviesById(id)
+}
+async function getRelatedMoviesById(id) {
+	const { data } = await api(`movie/${id}/similar`)
+	const relatedMovies = data.results
+	createMovies(relatedMovies, relatedMoviesContainer)
+	relatedMoviesContainer.scrollTo(0, 0)
+}
+
+export {
+	getTrendingCategoriesPreview,
+	getTrendingMoviesPreview,
+	getMoviesByCategory,
+	getMoviesBySearch,
+	getTrendingMovies,
+	getMovieById,
+}
--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,9 @@
 import {
+	getMovieById,
+	getMoviesByCategory,
+	getMoviesBySearch,
 	getTrendingCategoriesPreview,
+	getTrendingMovies,
 	getTrendingMoviesPreview,
 } from './main.js'
 import {
@@ -12,6 +16,7 @@ import {
 	movieDetailSection,
 	searchForm,
 	searchFormBtn,
+	searchFormInput,
 	trendingBtn,
 	trendingPreviewSection,
 } from './nodes.js'
@@ -19,9 +24,31 @@ import {
 window.addEventListener('hashchange', navigator, false)
 window.addEventListener('DOMContentLoaded', navigator, false)
 
-searchFormBtn.addEventListener('click', () => (location.hash = '#search='))
-trendingBtn.addEventListener('click', () => (location.hash = '#trends'))
-arrowBtn.addEventListener('click', () => (location.hash = '#home'))
+searchFormBtn.addEventListener('click', () => {
+	searchMovie()
+})
+
+trendingBtn.addEventListener('click', () => {
+	location.hash = '#trends'
+})
+arrowBtn.addEventListener('click', () => {
+	history.back()
+})
+searchForm.addEventListener('submit', (e) => e.preventDefault())
+searchForm.addEventListener('keyup', (e) => {
+	if (e.key === 'Enter') {
+		searchMovie()
+	}
+})
+function searchMovie() {
+	const searchValue = searchFormInput.value
+	if (searchValue.length < 1) {
+		searchFormInput.setAttribute('placeholder', 'Escribe algo...')
+		return
+	}
+	location.hash = `#search=${searchValue}`
+}
+
 function navigator() {
 	if (location.hash.startsWith('#trends')) {
 		trendsPage()
@@ -34,6 +61,7 @@ function navigator() {
 	} else {
 		homePage()
 	}
+	window.scrollTo(0, 0)
 }
 
 function homePage() {
@@ -72,6 +100,11 @@ function categoriesPage() {
 	categoriesPreviewSection.classList.add('inactive')
 	genericSection.classList.remove('inactive')
 	movieDetailSection.classList.add('inactive')
+
+	const [_, categoryData] = location.hash.split('=')
+	const [categoryId, categoryName] = categoryData.split('-')
+	headerCategoryTitle.textContent = decodeURI(categoryName)
+	getMoviesByCategory(categoryId)
 }
 function movieDetailsPage() {
 	console.log('movies')
@@ -88,6 +121,10 @@ function movieDetailsPage() {
 	categoriesPreviewSection.classList.add('inactive')
 	genericSection.classList.add('inactive')
 	movieDetailSection.classList.remove('inactive')
+
+	const [_, movieId] = location.hash.split('=')
+	console.log(decodeURI(movieId))
+	getMovieById(decodeURI(movieId))
 }
 function searchPage() {
 	console.log('search')
@@ -98,13 +135,17 @@ function searchPage() {
 	arrowBtn.classList.remove('header-arrow--white')
 
 	headerTitle.classList.add('inactive')
-	headerCategoryTitle.classList.remove('inactive')
+	headerCategoryTitle.classList.add('inactive')
 	searchForm.classList.remove('inactive')
 
 	trendingPreviewSection.classList.add('inactive')
 	categoriesPreviewSection.classList.add('inactive')
 	genericSection.classList.remove('inactive')
 	movieDetailSection.classList.add('inactive')
+
+	const [_, query] = location.hash.split('=')
+	console.log(decodeURI(query))
+	getMoviesBySearch(decodeURI(query))
 }
 function trendsPage() {
 	console.log('trends')
@@ -122,4 +163,7 @@ function trendsPage() {
 	categoriesPreviewSection.classList.add('inactive')
 	genericSection.classList.remove('inactive')
 	movieDetailSection.classList.add('inactive')
+
+	headerCategoryTitle.textContent = 'Tendencias'
+	getTrendingMovies()
 }
